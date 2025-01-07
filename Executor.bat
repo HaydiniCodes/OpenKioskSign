c75652f29d852e94f0d7c809c730b424e26ed3b7
HaydiniCodes <160955918+HaydiniCodes@users.noreply.github.com>
2025-01-07 14:47:25 -0600

    Update Executor.bat

--- Executor.bat
+++ Executor.bat
@@ -1,8 +1,2 @@
 @echo off
-:: Path to the PowerShell script
-set "ps1_script=C:\path\to\signage_script.ps1"
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%ps1_script%"
-
-pause
-
+powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Scripts\SignPlayer.ps1"
